d5f7be37517afaed2f856a51a5b8d3b291743714
SOPAN pitekar <sopanpitekar@hotmail.com>
2020-11-23 22:25:28 +0530

    lll

--- Gorakshnath Billing System/UI/frmUserDashboard.cs
+++ Gorakshnath Billing System/UI/frmUserDashboard.cs
@@ -21,6 +21,6 @@ namespace Gorakshnath_Billing_System
         private void frmUserDashboard_Load(object sender, EventArgs e)
         {
             label5.Text = frmLogin.loggedIn;
-        }
+        }//....
     }
 }
