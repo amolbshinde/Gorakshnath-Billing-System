dfe62d29694b3ba96a65e7089fd0a64f62df22e4
sopan <sopan@DESKTOP-1B4RFG9>
2020-11-30 20:25:06 +0530

    hello

--- Gorakshnath Billing System/UI/frmSales.cs
+++ Gorakshnath Billing System/UI/frmSales.cs
@@ -186,6 +186,7 @@ namespace Gorakshnath_Billing_System.UI
             {
               //  int productCount;
                 //productCount = dgvAddedProduct.Rows.Count;
+                //need to changes
                 for (int rows = 0; rows < dgvAddedProduct.Rows.Count; rows++)
                 {
                     pname = dgvAddedProduct.Rows[rows].Cells["Product Name"].Value.ToString();
