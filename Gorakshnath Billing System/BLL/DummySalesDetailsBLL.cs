77f0132b660c4a2e48c47d853738c7e4d9af9f36
Amol Balasaheb Shinde <amols8087@outlook.com>
2020-12-03 22:13:23 +0530

    DummySalesDetails Save code added

---
+++ Gorakshnath Billing System/BLL/DummySalesDetailsBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorakshnath_Billing_System.BLL
+{
+    class DummySalesDetailsBLL
+    {
+        public int Sales_ID { get; set; }
+        public int Invoice_No { get; set; }
+        public int Product_ID { get; set; }
+        public int Cust_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Unit { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Discount_Per { get; set; }
+        public string GST_Type { get; set; }
+        public decimal GST_Per { get; set; }
+        public decimal Total { get; set; }
+    }
+}
