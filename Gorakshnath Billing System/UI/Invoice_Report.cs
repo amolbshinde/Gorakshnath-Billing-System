a658715433b8e6c745380ac4759786a14371387e
Amol Balasaheb Shinde <amols8087@outlook.com>
2020-12-07 13:44:09 +0530

    ll

--- Gorakshnath Billing System/UI/Invoice_Report.cs
+++ Gorakshnath Billing System/UI/Invoice_Report.cs
@@ -31,21 +31,21 @@ namespace Gorakshnath_Billing_System.UI
 
         private void Invoice_Report_Load(object sender, EventArgs e)
         {
-
+            /*
             if (conn.State == ConnectionState.Closed)
                 conn.Open();
             SqlDataAdapter da = new SqlDataAdapter("SelectTransaction", conn);
             da.SelectCommand.CommandType = CommandType.StoredProcedure;
-            da.SelectCommand.Parameters.AddWithValue("@Sales_ID", 1);
+            da.SelectCommand.Parameters.AddWithValue("@Invoice_No", 2);
             DataTable dt = new DataTable();
             da.Fill(dt);
             conn.Close();
             Report_Generator.CrystalReport.crptInvoice crpt = new Report_Generator.CrystalReport.crptInvoice();
-            crpt.Database.Tables["Challan_Transaction"].SetDataSource(dt);
+            crpt.Database.Tables["Challan_Transactions"].SetDataSource(dt);
             crpt.Database.Tables["Challan_Transaction_Details"].SetDataSource(dt); //SetDataSource("Challan_Transaction_Details");
             crvViewer.ReportSource = null;
             crvViewer.ReportSource = crpt;
-
+            */
 
             /* Report_Generator.CrystalReport.crptInvoice crpt = new Report_Generator.CrystalReport.crptInvoice();
              crvViewer.ReportSource = null;
@@ -60,20 +60,23 @@ namespace Gorakshnath_Billing_System.UI
 
         private void btnPrintInvoice_Click(object sender, EventArgs e)
         {
-           /* if (conn.State==ConnectionState.Closed)
-            conn.Open();
-            SqlDataAdapter da = new SqlDataAdapter("SelectTransaction",conn);
-            da.SelectCommand.CommandType = CommandType.StoredProcedure;
-            da.SelectCommand.Parameters.AddWithValue("@Sales_ID",1);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            conn.Close();
+            /* if (conn.State==ConnectionState.Closed)
+             conn.Open();
+             SqlDataAdapter da = new SqlDataAdapter("SelectTransaction",conn);
+             da.SelectCommand.CommandType = CommandType.StoredProcedure;
+             da.SelectCommand.Parameters.AddWithValue("@Sales_ID",1);
+             DataTable dt = new DataTable();
+             da.Fill(dt);
+             conn.Close();
+             Report_Generator.CrystalReport.crptInvoice crpt = new Report_Generator.CrystalReport.crptInvoice();
+             crpt.Database.Tables["Challan_Transaction"].SetDataSource(dt);
+             crvViewer.ReportSource = null;
+             crvViewer.ReportSource = crpt;
+
+             */
             Report_Generator.CrystalReport.crptInvoice crpt = new Report_Generator.CrystalReport.crptInvoice();
-            crpt.Database.Tables["Challan_Transaction"].SetDataSource(dt);
             crvViewer.ReportSource = null;
             crvViewer.ReportSource = crpt;
-
-            */
 
         }
     }
