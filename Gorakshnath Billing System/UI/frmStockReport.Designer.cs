909e5fe53d863f97613e224e24cbbf0b306a8428
sopan <sopan@DESKTOP-1B4RFG9>
2020-12-14 14:09:30 +0530

    DSDADSAS

--- Gorakshnath Billing System/UI/frmStockReport.Designer.cs
+++ Gorakshnath Billing System/UI/frmStockReport.Designer.cs
@@ -30,8 +30,6 @@ namespace Gorakshnath_Billing_System.UI
         private void InitializeComponent()
         {
             this.comboSearchBy = new System.Windows.Forms.ComboBox();
-            this.textKeyword = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -56,36 +54,11 @@ namespace Gorakshnath_Billing_System.UI
             this.comboSearchBy.Items.AddRange(new object[] {
             "Product_Group",
             "Product_Brand"});
-            this.comboSearchBy.Location = new System.Drawing.Point(405, 27);
-            this.comboSearchBy.Margin = new System.Windows.Forms.Padding(4);
+            this.comboSearchBy.Location = new System.Drawing.Point(102, 22);
             this.comboSearchBy.Name = "comboSearchBy";
-            this.comboSearchBy.Size = new System.Drawing.Size(229, 33);
+            this.comboSearchBy.Size = new System.Drawing.Size(173, 28);
             this.comboSearchBy.TabIndex = 0;
             this.comboSearchBy.SelectedIndexChanged += new System.EventHandler(this.comboSearchBy_SelectedIndexChanged);
-            // 
-            // textKeyword
-            // 
-            this.textKeyword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textKeyword.Location = new System.Drawing.Point(99, 29);
-            this.textKeyword.Margin = new System.Windows.Forms.Padding(4);
-            this.textKeyword.Name = "textKeyword";
-            this.textKeyword.Size = new System.Drawing.Size(189, 32);
-            this.textKeyword.TabIndex = 1;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.FlatAppearance.BorderSize = 2;
-            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.ForeColor = System.Drawing.Color.Teal;
-            this.btnSearch.Location = new System.Drawing.Point(10, 26);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(81, 37);
-            this.btnSearch.TabIndex = 2;
-            this.btnSearch.Text = "All ";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // panel1
             // 
@@ -96,13 +69,10 @@ namespace Gorakshnath_Billing_System.UI
             this.panel1.Controls.Add(this.comboBox2);
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.btnSearch);
             this.panel1.Controls.Add(this.comboSearchBy);
-            this.panel1.Controls.Add(this.textKeyword);
-            this.panel1.Location = new System.Drawing.Point(3, 1);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1415, 89);
+            this.panel1.Size = new System.Drawing.Size(1248, 72);
             this.panel1.TabIndex = 3;
             // 
             // button3
@@ -111,10 +81,9 @@ namespace Gorakshnath_Billing_System.UI
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.Teal;
-            this.button3.Location = new System.Drawing.Point(1034, 24);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(635, 17);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 37);
+            this.button3.Size = new System.Drawing.Size(81, 30);
             this.button3.TabIndex = 7;
             this.button3.Text = "Product Name";
             this.button3.UseVisualStyleBackColor = true;
@@ -125,10 +94,9 @@ namespace Gorakshnath_Billing_System.UI
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Teal;
-            this.button2.Location = new System.Drawing.Point(651, 26);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(304, 20);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 37);
+            this.button2.Size = new System.Drawing.Size(78, 30);
             this.button2.TabIndex = 5;
             this.button2.Text = "Brand";
             this.button2.UseVisualStyleBackColor = true;
@@ -140,10 +108,9 @@ namespace Gorakshnath_Billing_System.UI
             this.comboBox2.Items.AddRange(new object[] {
             "Product_Group",
             "Product_Brand"});
-            this.comboBox2.Location = new System.Drawing.Point(1150, 26);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Location = new System.Drawing.Point(738, 17);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(247, 33);
+            this.comboBox2.Size = new System.Drawing.Size(186, 28);
             this.comboBox2.TabIndex = 6;
             // 
             // comboBox1
@@ -153,10 +120,9 @@ namespace Gorakshnath_Billing_System.UI
             this.comboBox1.Items.AddRange(new object[] {
             "Product_Group",
             "Product_Brand"});
-            this.comboBox1.Location = new System.Drawing.Point(763, 26);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Location = new System.Drawing.Point(405, 19);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(263, 33);
+            this.comboBox1.Size = new System.Drawing.Size(198, 28);
             this.comboBox1.TabIndex = 4;
             // 
             // button1
@@ -165,10 +131,9 @@ namespace Gorakshnath_Billing_System.UI
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Teal;
-            this.button1.Location = new System.Drawing.Point(307, 26);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(16, 22);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.Size = new System.Drawing.Size(68, 30);
             this.button1.TabIndex = 3;
             this.button1.Text = "Group";
             this.button1.UseVisualStyleBackColor = true;
@@ -176,20 +141,18 @@ namespace Gorakshnath_Billing_System.UI
             // panel2
             // 
             this.panel2.Controls.Add(this.dgvStockReport);
-            this.panel2.Location = new System.Drawing.Point(3, 144);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Location = new System.Drawing.Point(2, 117);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1415, 475);
+            this.panel2.Size = new System.Drawing.Size(1061, 386);
             this.panel2.TabIndex = 4;
             // 
             // dgvStockReport
             // 
             this.dgvStockReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStockReport.Location = new System.Drawing.Point(21, 16);
-            this.dgvStockReport.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvStockReport.Location = new System.Drawing.Point(16, 13);
             this.dgvStockReport.Name = "dgvStockReport";
             this.dgvStockReport.RowHeadersWidth = 51;
-            this.dgvStockReport.Size = new System.Drawing.Size(1364, 446);
+            this.dgvStockReport.Size = new System.Drawing.Size(1023, 362);
             this.dgvStockReport.TabIndex = 0;
             // 
             // button4
@@ -198,10 +161,9 @@ namespace Gorakshnath_Billing_System.UI
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.Teal;
-            this.button4.Location = new System.Drawing.Point(1162, 98);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(872, 80);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(124, 37);
+            this.button4.Size = new System.Drawing.Size(93, 30);
             this.button4.TabIndex = 5;
             this.button4.Text = "Search";
             this.button4.UseVisualStyleBackColor = true;
@@ -212,10 +174,9 @@ namespace Gorakshnath_Billing_System.UI
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.ForeColor = System.Drawing.Color.Teal;
-            this.button5.Location = new System.Drawing.Point(1294, 99);
-            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Location = new System.Drawing.Point(970, 80);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(124, 37);
+            this.button5.Size = new System.Drawing.Size(93, 30);
             this.button5.TabIndex = 6;
             this.button5.Text = "Reset";
             this.button5.UseVisualStyleBackColor = true;
@@ -226,10 +187,9 @@ namespace Gorakshnath_Billing_System.UI
             this.btnDate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDate.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDate.ForeColor = System.Drawing.Color.Teal;
-            this.btnDate.Location = new System.Drawing.Point(250, 99);
-            this.btnDate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDate.Location = new System.Drawing.Point(188, 80);
             this.btnDate.Name = "btnDate";
-            this.btnDate.Size = new System.Drawing.Size(292, 37);
+            this.btnDate.Size = new System.Drawing.Size(219, 30);
             this.btnDate.TabIndex = 8;
             this.btnDate.UseVisualStyleBackColor = true;
             // 
@@ -239,32 +199,29 @@ namespace Gorakshnath_Billing_System.UI
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.ForeColor = System.Drawing.Color.Teal;
-            this.button6.Location = new System.Drawing.Point(24, 99);
-            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Location = new System.Drawing.Point(18, 80);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(218, 37);
+            this.button6.Size = new System.Drawing.Size(164, 30);
             this.button6.TabIndex = 9;
             this.button6.Text = "Stock available as on :";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // frmStockReport
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1478, 693);
+            this.ClientSize = new System.Drawing.Size(1150, 563);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.btnDate);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmStockReport";
             this.Text = "Stock Report";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmStockReport_Load);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvStockReport)).EndInit();
             this.ResumeLayout(false);
@@ -274,8 +231,6 @@ namespace Gorakshnath_Billing_System.UI
         #endregion
 
         private System.Windows.Forms.ComboBox comboSearchBy;
-        private System.Windows.Forms.TextBox textKeyword;
-        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView dgvStockReport;
