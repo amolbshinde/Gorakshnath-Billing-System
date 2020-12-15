b081f6c490126c886f12981b207497c3ed4685bc
sopan <sopan@DESKTOP-1B4RFG9>
2020-12-15 09:23:49 +0530

    dfdfdf

--- Gorakshnath Billing System/UI/frmInvoiceCrpt.cs
+++ Gorakshnath Billing System/UI/frmInvoiceCrpt.cs
@@ -8,10 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
 namespace Gorakshnath_Billing_System.UI
 {
     public partial class frmInvoiceCrpt : Form
     {
+        ReportDocument cryRpt;
+
         int GetInvoice;
         public frmInvoiceCrpt(int InvoiceNo)
         {
@@ -37,11 +42,46 @@ namespace Gorakshnath_Billing_System.UI
             crptInvoice.SetParameterValue("@Invoice_No", GetInvoice.ToString());
             crptInvoiceViewer.ReportSource = crptInvoice;
 
+            cryRpt = new ReportDocument();
+            cryRpt.Load("C:\\Users\\sopan\\Documents\\CrystalReport1.rpt");
+            crptInvoiceViewer.ReportSource = cryRpt;
+            crptInvoiceViewer.Refresh();
+
+            try
+            {
+                ExportOptions CrExportOptions;
+                DiskFileDestinationOptions CrDiskFileDestinationOptions = new DiskFileDestinationOptions();
+                PdfRtfWordFormatOptions CrFormatTypeOptions = new PdfRtfWordFormatOptions();
+                CrDiskFileDestinationOptions.DiskFileName = "c:\\csharp.net-informations.pdf";
+                CrExportOptions = cryRpt.ExportOptions;
+                {
+                    CrExportOptions.ExportDestinationType = ExportDestinationType.DiskFile;
+                    CrExportOptions.ExportFormatType = ExportFormatType.PortableDocFormat;
+                    CrExportOptions.DestinationOptions = CrDiskFileDestinationOptions;
+                    CrExportOptions.FormatOptions = CrFormatTypeOptions;
+                }
+                cryRpt.Export();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+
         }
 
         private void crptInvoiceViewer_Load(object sender, EventArgs e)
         {
 
         }
+
+        //for storing crestal report
+
+
+
+
+
+
     }
 }
