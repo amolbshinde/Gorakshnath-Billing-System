736930fa9f748135a3930f94d7227855cf3745da
sopan <sopan@DESKTOP-1B4RFG9>
2020-12-19 16:05:17 +0530

    formhallan reportdesaingh

--- Gorakshnath Billing System/UI/frmChallanReport.Designer.cs
+++ Gorakshnath Billing System/UI/frmChallanReport.Designer.cs
@@ -171,7 +171,7 @@ namespace Gorakshnath_Billing_System.UI
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1300, 31);
+            this.panel3.Size = new System.Drawing.Size(1149, 31);
             this.panel3.TabIndex = 17;
             // 
             // label1
@@ -203,7 +203,7 @@ namespace Gorakshnath_Billing_System.UI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 788);
+            this.ClientSize = new System.Drawing.Size(1149, 655);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
