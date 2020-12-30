2843ff6035ee5ffe39cec25e2846ef5694be51e1
sopan <sopan@DESKTOP-1B4RFG9>
2020-12-30 15:43:28 +0530

    sasasas

---
+++ Gorakshnath Billing System/DAL/SalesPaymentDetailsDAL.cs
@@ -0,0 +1,16 @@
+﻿using Gorakshnath_Billing_System.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorakshnath_Billing_System.DAL
+{
+    class SalesPaymentDetailsDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+    }
+}
