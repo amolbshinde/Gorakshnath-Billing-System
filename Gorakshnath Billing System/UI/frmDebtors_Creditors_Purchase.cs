05866f984190883433eec347165d0024018aa76c
sopan <sopan@DESKTOP-1B4RFG9>
2020-12-31 15:09:53 +0530

    asasa

---
+++ Gorakshnath Billing System/UI/frmDebtors_Creditors_Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gorakshnath_Billing_System.UI
+{
+    public partial class frmDebtors_Creditors_Purchase : Form
+    {
+        public frmDebtors_Creditors_Purchase()
+        {
+            InitializeComponent();
+        }
+    }
+}
