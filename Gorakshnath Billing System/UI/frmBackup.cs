b11b8eaa969c2598709b8f768244884c18537feb
Amol Balasaheb Shinde <amols8087@outlook.com>
2021-04-24 13:29:07 +0530

    validation added to backup and restore

--- Gorakshnath Billing System/UI/frmBackup.cs
+++ Gorakshnath Billing System/UI/frmBackup.cs
@@ -43,14 +43,21 @@ namespace Gorakshnath_Billing_System.UI
 
         {
             try
-            {                
-                con.Open();
-                string query = "BACKUP DATABASE ["+con.Database+"] TO  DISK = N'"+ txtPath .Text+ "\\"+con.Database +".bak'";
-                //WITH NOFORMAT, NOINIT,  NAME = N'AnyStore-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.ExecuteNonQuery();
-                con.Close();
-                MessageBox.Show("Backup Completed Sucesfully");
+            {
+                if (txtPath.Text != "")
+                {
+                    con.Open();
+                    string query = "BACKUP DATABASE [" + con.Database + "] TO  DISK = N'" + txtPath.Text + "\\" + con.Database + ".bak'";
+                    //WITH NOFORMAT, NOINIT,  NAME = N'AnyStore-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Backup Completed Sucesfully");
+                }
+                else
+                {
+                    MessageBox.Show("Please select backup location!!");
+                }
 
 
             }
@@ -69,27 +76,33 @@ namespace Gorakshnath_Billing_System.UI
 
             try
             {
-                con.Open();
-                string get_db_offline = "alter database[Anystore] set single_user with rollback immediate";
-                SqlCommand cmd1 = new SqlCommand(get_db_offline,con);
-                cmd1.ExecuteNonQuery();;
-                con.Close();
+                if (txtRestore.Text != "")
+                {
+                    con.Open();
+                    string get_db_offline = "alter database[Anystore] set single_user with rollback immediate";
+                    SqlCommand cmd1 = new SqlCommand(get_db_offline, con);
+                    cmd1.ExecuteNonQuery(); ;
+                    con.Close();
 
-                con.Open();
-                string query = "Use [Master] RESTORE DATABASE [" + con.Database + "] FROM  DISK = N'"+txtRestore.Text+ "'WITH REPLACE;";
-                //WITH NOFORMAT, NOINIT,  NAME = N'AnyStore-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
-                SqlCommand cmd = new SqlCommand(query, con);                               
-                cmd.ExecuteNonQuery();
-                con.Close();
+                    con.Open();
+                    string query = "Use [Master] RESTORE DATABASE [" + con.Database + "] FROM  DISK = N'" + txtRestore.Text + "'WITH REPLACE;";
+                    //WITH NOFORMAT, NOINIT,  NAME = N'AnyStore-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
 
-                MessageBox.Show("Restore Completed Sucesfully");
-                con.Open();
-                string get_db_online = "ALTER DATABASE [Anystore] SET multi_user";
-                SqlCommand cmd2 = new SqlCommand(get_db_online, con);
-                cmd2.ExecuteNonQuery();
-                con.Close();
+                    MessageBox.Show("Restore Completed Sucesfully");
+                    con.Open();
+                    string get_db_online = "ALTER DATABASE [Anystore] SET multi_user";
+                    SqlCommand cmd2 = new SqlCommand(get_db_online, con);
+                    cmd2.ExecuteNonQuery();
+                    con.Close();
 
-
+                }
+                else
+                {
+                    MessageBox.Show("Please select backup file for Database Restore ..!");
+                }
             }
             catch (Exception ex)
             {
