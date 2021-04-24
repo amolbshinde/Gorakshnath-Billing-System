cfd842143ddcbcc731b22a80bc48c3d3e5debb2a
Amol Balasaheb Shinde <amols8087@outlook.com>
2021-04-24 12:27:33 +0530

    Backup and Restore Completed Succesfully. :)

--- Gorakshnath Billing System/UI/frmBackup.cs
+++ Gorakshnath Billing System/UI/frmBackup.cs
@@ -14,16 +14,22 @@ using System.Data.SqlClient;
 
 namespace Gorakshnath_Billing_System.UI
 {
+#pragma warning disable IDE1006 // Naming Styles
     public partial class frmBackup : Form
+#pragma warning restore IDE1006 // Naming Styles
     {
         public frmBackup()
         {
             InitializeComponent();
         }
 
+
         static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
         SqlConnection con = new SqlConnection(myconnstrng);
+
         private void button1_Click(object sender, EventArgs e)
+
         {
             FolderBrowserDialog fd = new FolderBrowserDialog();
             if(fd.ShowDialog()==DialogResult.OK)
@@ -34,6 +40,7 @@ namespace Gorakshnath_Billing_System.UI
         }
 
         private void button2_Click(object sender, EventArgs e)
+
         {
             try
             {                
@@ -55,50 +62,56 @@ namespace Gorakshnath_Billing_System.UI
             
         }
 
+
         private void button3_Click(object sender, EventArgs e)
+
         {
 
             try
             {
-                /*con.Open();
+                con.Open();
                 string get_db_offline = "alter database[Anystore] set single_user with rollback immediate";
                 SqlCommand cmd1 = new SqlCommand(get_db_offline,con);
                 cmd1.ExecuteNonQuery();;
-                con.Close();*/
+                con.Close();
 
                 con.Open();
-                string query = "RESTORE DATABASE [" + con.Database + "] FROM  DISK = N'"+txtRestore.Text+"' WITH  FILE = 1;";
+                string query = "Use [Master] RESTORE DATABASE [" + con.Database + "] FROM  DISK = N'"+txtRestore.Text+ "'WITH REPLACE;";
                 //WITH NOFORMAT, NOINIT,  NAME = N'AnyStore-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
                 SqlCommand cmd = new SqlCommand(query, con);                               
                 cmd.ExecuteNonQuery();
                 con.Close();
 
                 MessageBox.Show("Restore Completed Sucesfully");
-                /*con.Open();
+                con.Open();
                 string get_db_online = "ALTER DATABASE [Anystore] SET multi_user";
                 SqlCommand cmd2 = new SqlCommand(get_db_online, con);
                 cmd2.ExecuteNonQuery();
-                con.Close();*/
+                con.Close();
 
 
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-               /* con.Open();
+                con.Open();
                 string get_db_online = "ALTER DATABASE [Anystore] SET multi_user";
                 SqlCommand cmd2 = new SqlCommand(get_db_online, con);
-                cmd2.ExecuteNonQuery();*/
+                cmd2.ExecuteNonQuery();
                 con.Close();
 
             }
 
         }
 
+#pragma warning disable IDE1006 // Naming Styles
         private void button4_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006 // Naming Styles
         {
 
+#pragma warning disable IDE0017 // Simplify object initialization
             OpenFileDialog ofd = new OpenFileDialog();
+#pragma warning restore IDE0017 // Simplify object initialization
             ofd.Filter="Backup File(*.bak)|*.bak";
             ofd.Title = "Select Backup file";
 
