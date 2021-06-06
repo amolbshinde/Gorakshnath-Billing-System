19e470538c203a2587228795f62e7a7faaf8d606
Amol Balasaheb Shinde <amols8087@outlook.com>
2021-06-06 22:08:40 +0530

    kk

--- Gorakshnath Billing System/UI/frmDummySales.Designer.cs
+++ Gorakshnath Billing System/UI/frmDummySales.Designer.cs
@@ -341,10 +341,10 @@ namespace Gorakshnath_Billing_System.UI
             // 
             // textSearchItems
             // 
-            this.textSearchItems.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSearchItems.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textSearchItems.Location = new System.Drawing.Point(104, 51);
             this.textSearchItems.Name = "textSearchItems";
-            this.textSearchItems.Size = new System.Drawing.Size(427, 30);
+            this.textSearchItems.Size = new System.Drawing.Size(427, 27);
             this.textSearchItems.TabIndex = 1;
             this.textSearchItems.Text = "Select Item";
             this.textSearchItems.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textSearchItems_KeyUp);
