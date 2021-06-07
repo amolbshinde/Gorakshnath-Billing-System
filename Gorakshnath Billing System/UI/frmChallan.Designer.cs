bc2274eed1a401185051f687e44ec11443d52252
sopan <sopan@DESKTOP-1B4RFG9>
2021-06-07 18:31:41 +0530

    hett

--- Gorakshnath Billing System/UI/frmChallan.Designer.cs
+++ Gorakshnath Billing System/UI/frmChallan.Designer.cs
@@ -89,7 +89,6 @@ namespace Gorakshnath_Billing_System.UI
             this.button1 = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.textIgst = new System.Windows.Forms.TextBox();
             this.label36 = new System.Windows.Forms.Label();
@@ -797,19 +796,18 @@ namespace Gorakshnath_Billing_System.UI
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Teal;
-            this.button1.Location = new System.Drawing.Point(346, 191);
+            this.button1.Location = new System.Drawing.Point(290, 191);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 32);
+            this.button1.Size = new System.Drawing.Size(150, 32);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Save ";
+            this.button1.Text = "Save and Print";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel6
             // 
             this.panel6.Controls.Add(this.button3);
-            this.panel6.Controls.Add(this.button2);
             this.panel6.Controls.Add(this.btnClear);
             this.panel6.Controls.Add(this.textIgst);
             this.panel6.Controls.Add(this.label36);
@@ -844,21 +842,6 @@ namespace Gorakshnath_Billing_System.UI
             this.button3.Text = "Exit";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button2
-            // 
-            this.button2.FlatAppearance.BorderSize = 2;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.BlueViolet;
-            this.button2.Location = new System.Drawing.Point(267, 191);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(76, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = " Print";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnClear
             // 
@@ -1120,7 +1103,6 @@ namespace Gorakshnath_Billing_System.UI
         private System.Windows.Forms.TextBox textIgst;
         private System.Windows.Forms.Label label36;
         private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ComboBox comboSearchCust;
         private System.Windows.Forms.ComboBox comboContact;
